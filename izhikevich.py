bf1cec12c294dbbe655d38ddab8196ce7811d225
Thomas McCall <tommccall8829@gmail.com>
2017-07-20 16:54:50 -0400

    Added .gitignore and changed izhikevich

--- izhikevich.py
+++ izhikevich.py
@@ -1,14 +1,7 @@
-from brian2 import *
+import numpy as np
 
-""" All values and equations were taken from https://www.izhikevich.org/publications/spikes.pdf
-unless otherwise specified """
+Ne = 800 # Excitatory neurons
+Ni = 200 # Inhibitory neurons
 
-
-a = 0.02 # Dimensionless
-b = 0.2 # Dimensionless
-c = -65*mV # Voltage to reset to after peak
-
-eqs = '''
-dv/dt = 0.04 * (v**2) + 5 * v + 140 - u + I : 1
-du/dt = a * (b * v - u) : 1
-'''
+re = np.random.rand(Ne, 1)
+ri = np.random.rand(Ni, 1)
